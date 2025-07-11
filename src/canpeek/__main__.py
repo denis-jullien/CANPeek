e050cf180710f353a2b30acbedbfa3f744d4cf05
Denis <denis.jullien@seabubbles.com>
2025-07-11 22:56:16 +0200

    remove CDockWidget deprecation warning

--- src/canpeek/__main__.py
+++ src/canpeek/__main__.py
@@ -1638,13 +1638,13 @@ class CANBusObserver(QMainWindow):
             "This is a DockArea which is always visible, even if it does not contain any DockWidgets."
         )
         label.setAlignment(Qt.AlignCenter)
-        central_dock_widget = QtAds.CDockWidget("CentralWidget")
+        central_dock_widget = QtAds.CDockWidget(self.dock_manager, "CentralWidget")
         central_dock_widget.setWidget(label)
         central_dock_widget.setFeature(QtAds.CDockWidget.NoTab, True)
         central_dock_area = self.dock_manager.setCentralWidget(central_dock_widget)
 
         self.project_explorer = ProjectExplorer(self.project, self)
-        explorer_dock = QtAds.CDockWidget("Project Explorer")
+        explorer_dock = QtAds.CDockWidget(self.dock_manager, "Project Explorer")
         explorer_dock.setWidget(self.project_explorer)
         project_aera = self.dock_manager.addDockWidget(
             QtAds.DockWidgetArea.LeftDockWidgetArea, explorer_dock
@@ -1653,22 +1653,22 @@ class CANBusObserver(QMainWindow):
         self.properties_panel = PropertiesPanel(
             self.project, self.project_explorer, self.interface_manager, self
         )
-        properties_dock = QtAds.CDockWidget("Properties")
+        properties_dock = QtAds.CDockWidget(self.dock_manager, "Properties")
         properties_dock.setWidget(self.properties_panel)
         self.dock_manager.addDockWidget(
             QtAds.DockWidgetArea.BottomDockWidgetArea, properties_dock, project_aera
         )
 
         # New docks for previously tabbed views
-        grouped_dock = QtAds.CDockWidget("Grouped View")
+        grouped_dock = QtAds.CDockWidget(self.dock_manager, "Grouped View")
         grouped_dock.setWidget(self.grouped_view)
         self.dock_manager.addDockWidgetTabToArea(grouped_dock, central_dock_area)
 
-        trace_dock = QtAds.CDockWidget("Trace View")
+        trace_dock = QtAds.CDockWidget(self.dock_manager, "Trace View")
         trace_dock.setWidget(self.trace_view_widget)
         self.dock_manager.addDockWidgetTabToArea(trace_dock, central_dock_area)
 
-        object_dictionary_dock = QtAds.CDockWidget("Object Dictionary")
+        object_dictionary_dock = QtAds.CDockWidget(self.dock_manager, "Object Dictionary")
         object_dictionary_dock.setWidget(self.object_dictionary_viewer)
         self.dock_manager.addDockWidgetTabToArea(
             object_dictionary_dock, central_dock_area
@@ -1684,13 +1684,13 @@ class CANBusObserver(QMainWindow):
         transmit_layout.addWidget(self.transmit_panel)
         transmit_layout.addWidget(self.signal_transmit_panel)
         self.signal_transmit_panel.setVisible(False)
-        transmit_dock = QtAds.CDockWidget("Transmit")
+        transmit_dock = QtAds.CDockWidget(self.dock_manager, "Transmit")
         transmit_dock.setWidget(transmit_container)
         transmit_aera = self.dock_manager.addDockWidget(
             QtAds.BottomDockWidgetArea, transmit_dock, central_dock_area
         )
 
-        nmt_sender_dock = QtAds.CDockWidget("NMT Sender")
+        nmt_sender_dock = QtAds.CDockWidget(self.dock_manager, "NMT Sender")
         nmt_sender_dock.setWidget(self.nmt_sender)
         self.dock_manager.addDockWidgetTabToArea(nmt_sender_dock, transmit_aera)
 
